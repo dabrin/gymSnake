ad88e56e2845dff0473340d13f7437b8f86d14d7
Pablo Rojas <pablo.rojas96@hotmail.com>
2020-05-28 22:50:44 -0600

    Interfaz gráfica

---
+++ public/contact.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Gym Snake</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    
+    <link rel="icon" type="image/png" href="images/logo.ico"/>
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+
+
+  <body>
+  
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    <div class="site-navbar-wrap bg-white">
+      <div class="site-navbar-top">
+        <div class="container py-2">
+          <div class="row align-items-center">
+            <div class="col-6">
+              <a href="#" class="p-2 pl-0"><span target="_blank" class="icon-twitter"></span></a>
+              <a href="https://www.facebook.com/GymSnake/" target="_blank" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+              <a href="#" class="p-2 pl-0"><span target="_blank" class="icon-linkedin"></span></a>
+              <a href="https://www.instagram.com/gym_snake_turrialba/"  target="_blank" class="p-2 pl-0"><span target="_blank" class="icon-instagram"></span></a>
+            </div>
+            <div class="col-6">
+              <div class="d-flex ml-auto">
+                <a href="#" class="d-flex align-items-center ml-auto mr-4">
+                  <span class="icon-envelope mr-2"></span>
+                  <span class="d-none d-md-inline-block">gymsnake@gmail.com</span>
+                </a>
+                <a href="tel://12912830302" class="d-flex align-items-center">
+                  <span class="icon-phone mr-2"></span>
+                  <span class="d-none d-md-inline-block">+506 8888 88 88</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+       <div class="site-navbar-wrap bg-white">
+      
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <div class="py-1">
+            <div class="row align-items-center">
+              <div class="col-4">
+              </div>
+              <div class="col-8">
+                <h2 class="mb-0 site-logo"><a href="index.html">Gym<strong>Snake</strong>  </a></h2>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+    <div class="site-blocks-cover inner-page overlay" style="background-image: url(images/about.jpeg);" >
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-7 text-center">
+            <h1 class="mb-5">Inscribete en la clase</h1>
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row">
+       
+          <div class="col-md-12 col-lg-8 mb-5">
+          
+            
+          
+            <form action="#" class="p-5 bg-white">
+
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label class="font-weight-bold" for="fullname">Nombre Completo</label>
+                  <input type="text" id="fullname" class="form-control" placeholder="Nombre Completo" required>
+                </div>
+              </div>
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <label class="font-weight-bold" for="email">Email</label>
+                  <input type="email" id="email" class="form-control" placeholder="ejemplo@mail.com">
+                </div>
+              </div>
+
+
+              <div class="row form-group">
+                <div class="col-md-12 mb-3 mb-md-0">
+                  <label class="font-weight-bold" for="phone">Número de Teléfono</label>
+                  <input type="text" id="phone" class="form-control" placeholder="Número de Teléfono">
+                </div>
+              </div>
+
+
+              <div class="row form-group">
+                <div class="col-md-12">
+                  <input type="submit" class="btn btn-primary pill px-4 mt-3 mt-md-0" id="agregar" value="Inscribirse" >
+                   
+                </div>
+              </div>
+
+  
+            </form>
+          </div>
+
+         
+        </div>
+        <table class="table table-hover table-sm" id=listado>
+          <thead>
+            <tr>
+            <!--  <th scope="col">#</th>-->
+              <th scope="col">Nombre</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Teléfono</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+         <!--     <th scope="row">1</th>-->
+              <td>Mark</td>
+              <td>Otto@mail.com</td>
+              <td>12345678</td>
+            </tr>
+            <tr>
+           <!--   <th scope="row">2</th>-->
+              <td>Jacob</td>
+              <td>Thornton@mail.com</td>
+              <td>98765432</td>
+            </tr>
+           
+          </tbody>
+        </table>
+      </div>
+      
+
+
+    </div>
+
+    <div class="col-sm-3 col-md-3 col-lg-3 text-md-right"><a href="index.html" class="btn btn-primary pill px-4 mt-3 mt-md-0"><span ><i class="fas fa-long-arrow-alt-left"></i></span> Volver </a></div>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        
+        <div class="row">
+       
+          <div class="col-lg-5">
+            <div class="row mb-5">
+              <div class="col-md-12"><h3 class="footer-heading mb-4 text-primary">Contáctenos </h3></div>
+              <div class="col-md-6">
+                <p>San Rafael <br> Turrialba</p>    
+              </div>
+              <div class="col-md-6">
+                Tel. + (506) 88888888 <br>
+                Mail. gymsnake@gmail.com
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12"><h3 class="footer-heading mb-4 text-primary">Redes Sociales </h3></div>
+              <div class="col-md-12">
+                <p>
+                  <a href="#" class="p-2 pl-0"><span target="_blank" class="icon-twitter"></span></a>
+              <a href="https://www.facebook.com/GymSnake/" target="_blank" class="p-2 pl-0"><span class="icon-facebook"></span></a>
+              <a href="#" class="p-2 pl-0"><span target="_blank" class="icon-linkedin"></span></a>
+              <a href="https://www.instagram.com/gym_snake_turrialba/"  target="_blank" class="p-2 pl-0"><span target="_blank" class="icon-instagram"></span></a>
+           
+
+                </p>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+
+
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+            Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All Rights Reserved | This web page is made by Pablo Rojas 
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+    
+  </div>
+
+  
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
+
+
+
+
+<script>
+  document.querySelector("input[id=agregar]").addEventListener("click",function(e){
+    e.preventDefault();
+
+    const nombre=document.querySelector("input[id=fullname]");
+    const email=document.querySelector("input[id=email]");
+    const telefono=document.querySelector("input[id=phone]");
+
+    if(!nombre.value)
+    {
+        nombre.classList.add("error");
+        return;
+    }
+    nombre.classList.remove("error");
+
+    const tr=document.createElement("tr");
+ 
+    const tdNombre=document.createElement("td");
+    let txt=document.createTextNode(nombre.value);
+    tdNombre.appendChild(txt);
+    tdNombre.className="nombre";
+    //--------------------------------------
+
+ 
+    const tdEmail=document.createElement("td");
+     txt=document.createTextNode(email.value);
+    tdEmail.appendChild(txt);
+    tdEmail.className="email";
+    //--------------------------------------
+    
+ 
+    const tdTelefono=document.createElement("td");
+     txt=document.createTextNode(telefono.value);
+    tdTelefono.appendChild(txt);
+    tdTelefono.className="telefono";
+     //--------------------------------------
+
+     const tdRemove=document.createElement("td");
+    const buttonRemove=document.createElement("img");
+    buttonRemove.src="garbage.png";
+    buttonRemove.onclick=function () {
+        // elimina la columna
+        this.closest("tr").remove();
+ 
+        // Si no hay ningun elemento en la columna, escondemos la tabla
+        if(document.getElementById("listado").querySelector("tbody").querySelectorAll("tr").length==0)
+        {
+            document.getElementById("listado").classList.add("hide");
+        }
+    };
+    tdRemove.appendChild(buttonRemove);
+
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdTelefono);
+   // tr.appendChild(tdRemove);
+
+    const tbody=document.getElementById("listado").querySelector("tbody").appendChild(tr);
+ 
+ // eliminamos la clase que tiene oculta la tabla cando no hay ningun alumno
+ document.getElementById("listado").classList.remove("hide");
+
+ //limpiamos los valores del input
+ email.value="";
+ nombre.value="";
+ telefono.value="";
+ nombre.focus();
+
+  });
+  </script>
+  
