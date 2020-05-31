9af89ad105f7667b2fa2f0ee2044a2237c565da2
Pablo Rojas <pablo.rojas96@hotmail.com>
2020-05-31 00:17:20 -0600

    BD actualizada

--- Bases de Datos/script.sql
+++ Bases de Datos/script.sql
@@ -19,13 +19,11 @@ nombre varchar(300) not null,
 telefono varchar(8) not null
 );
 
-
 create table tipo_clase(
 id_tipo_clase int not null auto_increment primary key,
 nombre_tipo_clase varchar(300) not null,
 descripcion varchar(500)
 );
-
 
 create table clase(
 id_clase int auto_increment primary key,
@@ -74,3 +72,67 @@ id_admin int auto_increment primary key not null,
 usuario_admin varchar(300) not null,
 contrasena varchar(100) not null
 );
+
+
+-- ------------------------------- PROCEDIMIENTOS ALMACENADOS -------------------------------
+
+-- SP para el login
+DELIMITER $$
+CREATE DEFINER=`laboratorios`@`%` PROCEDURE `login`(user_ varchar(300), pass_ varchar(100))
+BEGIN
+	select id_admin from administrador where usuario_admin=user_ && contrasena=pass_;
+END
+DELIMITER ;
+
+-- SP para listar las clases
+DELIMITER $$
+CREATE DEFINER=`laboratorios`@`%` PROCEDURE `sp_listar_clases`()
+BEGIN
+	Select * from clase c
+    JOIN tipo_clase tc
+    ON c.id_tipo_clase = tc.id_tipo_clase;
+END
+DELIMITER ;
+
+
+-- SP listar las personas de cada clase
+DELIMITER $$
+CREATE DEFINER=`laboratorios`@`%` PROCEDURE `sp_listar_persona_clase`()
+BEGIN
+select nombre,telefono 
+from persona
+ join persona_clase 
+ on persona.id_persona= persona_clase.id_persona;
+END
+DELIMITER ;
+
+
+-- SP para listar los clientes
+DELIMITER $$
+CREATE DEFINER=`laboratorios`@`%` PROCEDURE `sp_listarClientes`()
+BEGIN
+	SELECT * FROM persona;
+END
+DELIMITER ;
+
+
+-- SP para insertar los clientes
+DELIMITER $$
+CREATE DEFINER=`laboratorios`@`%` PROCEDURE `sp_ingresar_clientes`(nombre_ varchar(100), telefono_ varchar(8), id_clase_ int)
+BEGIN
+	INSERT INTO persona (nombre,telefono)
+    values (nombre_,telefono_);
+    INSERT INTO persona_clase (id_persona , id_clase)
+    values(LAST_INSERT_ID(),id_clase_);
+END
+DELIMITER ;
+
+
+-- ------------------------------- PROCEDIMIENTOS ALMACENADOS -------------------------------
+
+
+
+
+
+
+
